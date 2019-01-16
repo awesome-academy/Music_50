cf5afc7086d654358e9df78d83a52acec417651f
QuangThao <huynh.quang.thao@framgia.com>
2019-01-16 08:17:45 +0700

    handle play pause event

--- app/src/main/java/com/framgia/music_50/service/ServiceContract.java
+++ app/src/main/java/com/framgia/music_50/service/ServiceContract.java
@@ -1,7 +1,14 @@
 package com.framgia.music_50.service;
 
+import com.framgia.music_50.data.model.Track;
+
 public interface ServiceContract {
     interface OnMediaPlayerChange{
+        void onTrackChange(Track track);
+
+        void onMediaPlayerStart();
+
+        void onMediaPlayerStateChange(boolean isPlaying);
     }
 
     interface OnMiniControllerChange{
