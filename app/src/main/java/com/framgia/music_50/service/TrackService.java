f61ee034ba93e10ba93a9a9b18ff7f4dfd02fec1
QuangThao <huynh.quang.thao@framgia.com>
2019-01-18 15:11:57 +0700

    create controller notification

--- app/src/main/java/com/framgia/music_50/service/TrackService.java
+++ app/src/main/java/com/framgia/music_50/service/TrackService.java
@@ -1,14 +1,25 @@
 package com.framgia.music_50.service;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.os.Binder;
+import android.os.Build;
 import android.os.IBinder;
 import android.os.Parcelable;
+import android.support.v4.app.NotificationCompat;
+import android.widget.RemoteViews;
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.target.NotificationTarget;
+import com.framgia.music_50.R;
 import com.framgia.music_50.data.model.Track;
+import com.framgia.music_50.screen.main.MainActivity;
 import com.framgia.music_50.utils.Constant;
 import com.framgia.music_50.utils.LoopType;
 import java.io.IOException;
@@ -20,6 +31,13 @@ public class TrackService extends Service
         implements MediaPlayer.OnPreparedListener, MediaPlayer.OnCompletionListener {
     private final static String EXTRA_TRACK_LIST = "EXTRA_TRACK_LIST";
     private final static String EXTRA_TRACK_POSITION = "EXTRA_TRACK_POSITION";
+    private final static String CHANNEL_NAME = "Y Music";
+    private final static String CHANNEL_ID = "CHANNEL_ID";
+    private final static String ACTION_PLAY_PAUSE = "ACTION_PLAY_PAUSE";
+    private final static String ACTION_SKIP_NEXT = "ACTION_SKIP_NEXT";
+    private final static String ACTION_SKIP_PREVIOUS = "ACTION_SKIP_PREVIOUS";
+    private final static int NOTIFICATION_ID = 2019;
+    private final static int PENDING_REQUEST_CODE = 0;
     private final int DEFAULT_POSITION = 0;
     private final int DEFAULT_VALUE_ONE = 1;
     private List<Track> mTracks;
@@ -32,6 +50,9 @@ public class TrackService extends Service
     private TrackBinder mTrackBinder;
     private int mLoopType;
     private boolean mIsShuffle;
+    private RemoteViews mRemoteViews;
+    private NotificationTarget mArtworkTarget;
+    private Notification mNotification;
 
     public static Intent getServiceIntent(Context context, List<Track> tracks, int position) {
         Intent intent = new Intent(context, TrackService.class);
@@ -45,20 +66,37 @@ public class TrackService extends Service
     public void onCreate() {
         super.onCreate();
         mTrackBinder = new TrackBinder();
+        createNotification();
     }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         if (intent != null) {
-            mTracks = intent.getParcelableArrayListExtra(EXTRA_TRACK_LIST);
-            mPosition = intent.getIntExtra(EXTRA_TRACK_POSITION, DEFAULT_POSITION);
-            if (mTracks != null) {
+            List<Track> tracks = intent.getParcelableArrayListExtra(EXTRA_TRACK_LIST);
+            if (tracks != null) {
+                mPosition = intent.getIntExtra(EXTRA_TRACK_POSITION, DEFAULT_POSITION);
+                mTracks = tracks;
                 mLoopType = LoopType.LOOP_ALL;
                 mIsShuffle = false;
                 mShuffleTracks = new ArrayList<>(mTracks);
                 mTemporaryTracks = new ArrayList<>(mTracks);
                 Collections.shuffle(mShuffleTracks);
                 play();
+            }
+            if (intent.getAction() != null) {
+                switch (intent.getAction()) {
+                    case ACTION_SKIP_PREVIOUS:
+                        updatePlayPauseNotification(false);
+                        skipPrevious();
+                        break;
+                    case ACTION_PLAY_PAUSE:
+                        playPauseTrack();
+                        break;
+                    case ACTION_SKIP_NEXT:
+                        updatePlayPauseNotification(false);
+                        skipNext();
+                        break;
+                }
             }
         }
         return START_NOT_STICKY;
@@ -96,6 +134,7 @@ public class TrackService extends Service
         if (mOnMediaPlayerChange != null) {
             mOnMediaPlayerChange.onTrackChange(track);
         }
+        updateNotificationUI(track);
     }
 
     @Override
@@ -106,6 +145,7 @@ public class TrackService extends Service
             mOnMediaPlayerChange.onMediaPlayerStart();
             mOnMediaPlayerChange.onMediaPlayerStateChange(true);
         }
+        updatePlayPauseNotification(true);
     }
 
     @Override
@@ -144,6 +184,7 @@ public class TrackService extends Service
                 mOnMediaPlayerChange.onMediaPlayerStateChange(true);
             }
         }
+        updatePlayPauseNotification(mMediaPlayer.isPlaying());
     }
 
     public void seekTo(int duration) {
@@ -206,6 +247,71 @@ public class TrackService extends Service
 
     public Track getCurrentTrack() {
         return mTracks.get(mPosition);
+    }
+
+    private void createNotification() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, CHANNEL_NAME,
+                    NotificationManager.IMPORTANCE_DEFAULT);
+            channel.setSound(null, null);
+            channel.enableVibration(false);
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+        mRemoteViews = new RemoteViews(getPackageName(), R.layout.notification_controller);
+        initNotificationAction();
+        Intent mainActivityIntent = new Intent(this, MainActivity.class);
+        mainActivityIntent.setFlags(
+                Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+        PendingIntent pendingIntent =
+                PendingIntent.getActivity(this, PENDING_REQUEST_CODE, mainActivityIntent,
+                        PendingIntent.FLAG_UPDATE_CURRENT);
+        mNotification =
+                new NotificationCompat.Builder(this, CHANNEL_ID).setSmallIcon(R.drawable.ic_music)
+                        .setCustomContentView(mRemoteViews)
+                        .setCustomBigContentView(mRemoteViews)
+                        .setContentIntent(pendingIntent)
+                        .build();
+        mArtworkTarget =
+                new NotificationTarget(getApplicationContext(), R.id.imageViewArtistNotification,
+                        mRemoteViews, mNotification, NOTIFICATION_ID);
+        startForeground(NOTIFICATION_ID, mNotification);
+    }
+
+    private void initNotificationAction() {
+        createAction(ACTION_PLAY_PAUSE, R.id.imageViewPlayPauseNotification);
+        createAction(ACTION_SKIP_NEXT, R.id.imageViewSkipNextNotification);
+        createAction(ACTION_SKIP_PREVIOUS, R.id.imageViewSkipPreviousNotification);
+    }
+
+    private void createAction(String action, int viewId) {
+        Intent intent = new Intent();
+        intent.setAction(action);
+        intent.setClass(getApplicationContext(), TrackService.class);
+        PendingIntent playPausePendingIntent =
+                PendingIntent.getService(getApplicationContext(), NOTIFICATION_ID, intent,
+                        PendingIntent.FLAG_UPDATE_CURRENT);
+        mRemoteViews.setOnClickPendingIntent(viewId, playPausePendingIntent);
+    }
+
+    private void updateNotificationUI(Track track) {
+        mRemoteViews.setTextViewText(R.id.textViewTrackTitleNotification, track.getTitle());
+        mRemoteViews.setTextViewText(R.id.textViewArtistNameNotification, track.getArtistName());
+        Glide.with(getApplicationContext())
+                .asBitmap()
+                .load(track.getArtworkUrl())
+                .into(mArtworkTarget);
+    }
+
+    private void updatePlayPauseNotification(boolean isPlaying) {
+        if (isPlaying) {
+            mRemoteViews.setImageViewResource(R.id.imageViewPlayPauseNotification,
+                    R.drawable.ic_pause);
+        } else {
+            mRemoteViews.setImageViewResource(R.id.imageViewPlayPauseNotification,
+                    R.drawable.ic_play);
+        }
+        startForeground(NOTIFICATION_ID, mNotification);
     }
 
     public class TrackBinder extends Binder {
